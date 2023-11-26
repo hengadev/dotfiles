407a9a527d8bf07e5ebb3106a0f7992ec50a846a
gary <henry.gary@hotmail.com>
2023-11-26 17:57:37 +0100

    fix: remove the golang nvim-dap config for nvim-dap-go

--- nvim/lua/plugins/dap/go.lua
+++ nvim/lua/plugins/dap/go.lua
@@ -1,42 +1,44 @@
+--  NOTE: Je vais me contenter de la configuration proposee par nvim-dap-go
+
 --  On trouve la config de : https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#ccrust-via-codelldb
 
-local M = {}
+-- local M = {}
+--
+-- function M.setup()
+--     local dap = require "dap"
+--     dap.adapters.delve = {
+--         type = 'server',
+--         port = '${port}',
+--         executable = {
+--             command = 'dlv',
+--             args = { 'dap', '-l', '127.0.0.1:${port}' },
+--         }
+--     }
 
-function M.setup()
-    local dap = require "dap"
-    dap.adapters.delve = {
-        type = 'server',
-        port = '${port}',
-        executable = {
-            command = 'dlv',
-            args = { 'dap', '-l', '127.0.0.1:${port}' },
-        }
-    }
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+--     dap.configurations.go = {
+--         {
+--             type = "delve",
+--             name = "Debug",
+--             request = "launch",
+--             program = "${file}"
+--         },
+--         {
+--             type = "delve",
+--             name = "Debug test", -- configuration for debugging test files
+--             request = "launch",
+--             mode = "test",
+--             program = "${file}"
+--         },
+--         -- works with go.mod packages and sub packages
+--         {
+--             type = "delve",
+--             name = "Debug test (go.mod)",
+--             request = "launch",
+--             mode = "test",
+--             program = "./${relativeFileDirname}"
+--         }
+--     }
+-- end
 
-    -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
-    dap.configurations.go = {
-        {
-            type = "delve",
-            name = "Debug",
-            request = "launch",
-            program = "${file}"
-        },
-        {
-            type = "delve",
-            name = "Debug test", -- configuration for debugging test files
-            request = "launch",
-            mode = "test",
-            program = "${file}"
-        },
-        -- works with go.mod packages and sub packages
-        {
-            type = "delve",
-            name = "Debug test (go.mod)",
-            request = "launch",
-            mode = "test",
-            program = "./${relativeFileDirname}"
-        }
-    }
-end
-
-return M
+-- return M
