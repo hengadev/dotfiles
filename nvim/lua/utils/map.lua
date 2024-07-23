d44467b2c4301d7a649b30c1ab7baee359f648b8
GaryHY <henry.gary@hotmail.com>
2024-07-23 20:34:38 +0200

    feat: add signature for util map function

--- nvim/lua/utils/map.lua
+++ nvim/lua/utils/map.lua
@@ -1,11 +1,13 @@
 local M = {}
 
--- setting key maps for the plugin
----a helper function first to help with the settings of the keymaps.
----@params:key string
----@params:func func()
----@params:desc string
+-- setting prefix for keymaps.
+---@param prefix string
 function M.set_prefix(prefix)
+-- setting keymaps for the plugin related to the prefix.
+---@param key string
+---@param cmd string|function
+---@param desc string
+---@param modes table|nil
     return function(key, cmd, desc, modes)
         if modes == nil then
             vim.keymap.set("n", key, cmd, { desc = prefix .. ": " .. desc })
