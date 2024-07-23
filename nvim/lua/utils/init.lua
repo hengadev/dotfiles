5e99bf2cf19c3b6074d9fde40ac242e6d9d3ef8d
GaryHY <henry.gary@hotmail.com>
2024-07-23 14:25:36 +0200

    feat: add map utils function to set keymaps

--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -296,4 +296,23 @@ function M.has(plugin)
     return require("lazy.core.config").plugins[plugin] ~= nil
 end
 
+
+-- setting key maps for the plugin
+---a helper function first to help with the settings of the keymaps.
+---@params:key string
+---@params:func func()
+---@params:desc string
+function M.map(prefix)
+    return function(key, cmd, desc, modes)
+        if modes == nil then
+            vim.keymap.set("n", key, cmd, { desc = prefix .. ": " .. desc })
+            return
+        end
+        for _, mode in pairs(modes) do
+            vim.keymap.set(mode, key, cmd, { desc = prefix .. ": " .. desc })
+        end
+    end
+end
+
+
 return M
