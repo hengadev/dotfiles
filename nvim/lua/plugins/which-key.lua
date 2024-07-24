671c89341af0f51868766c70080b93a3066c8ac4
GaryHY <henry.gary@hotmail.com>
2024-07-24 18:33:10 +0200

    feat: update which-key plugin with all the keymaps specs

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,85 +1,70 @@
--- TODO: Do the setup for that, make a good register for it.
+local makeKey = require("utils.makeKey")
+SetLegendaryKey = makeKey("Legendary")
+
 return {
     {
         "mrjones2014/legendary.nvim",
         keys = {
-            { "<C-S-p>",    "<cmd>Legendary<cr>", desc = "Legendary" },
-            { "<leader>hc", "<cmd>Legendary<cr>", desc = "Command Palette" },
+            SetLegendaryKey("<C-S-p>", "<cmd>Legendary<cr>", "Legendary" ),
+            SetLegendaryKey("<leader>cp", "<cmd>Legendary<cr>", "Command Palette" ),
         },
         opts = {
             which_key = { auto_register = true },
         },
     },
     {
-        "folke/which-key.nvim",
-        dependencies = {
-            "mrjones2014/legendary.nvim",
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        win = {
+            border = "single",        -- none, single, double, shadow
+            -- position = "bottom",      -- bottom, top
+            -- margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
+            padding = { 1, 1 }, -- extra window padding [top, right, bottom, left]
+            -- winblend = 0,
         },
-        -- the most recent versions causes the mapping to break...
-        version="2.1.0",
-        event = {"BufReadPre", "BufNewFile"},
-        config = function()
-            local wk = require "which-key"
-            wk.setup {
-                show_help = true,
-                plugins = { spelling = true },
-                key_labels = { ["<leader>"] = "SPC" },
-                triggers = "auto",
-                window = {
-                    border = "single",        -- none, single, double, shadow
-                    position = "bottom",      -- bottom, top
-                    margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
-                    padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
-                    winblend = 0,
-                },
-                layout = {
-                    height = { min = 4, max = 25 }, -- min and max height of the columns
-                    width = { min = 20, max = 50 }, -- min and max width of the columns
-                    spacing = 3,                    -- spacing between columns
-                    align = "left",                 -- align columns left, center or right
-                },
-            }
-            wk.register({
-                w = { "<cmd>update!<CR>", "Save" },
-                -- stylua: ignore
-                q = {
-                    name = "Quit",
-                    t = { "<cmd>tabclose<cr>", "Close Tab" },
-                },
-                c = { name = "+Command" },
-                d = { name = "+Debug" },
-                b = { name = "+Database",
-                    d = { name = "Toggle DadBodUI menu" },
-                },
-                h = { name = "+Gitsigns" },
-                g = { name = "+Git",
-                    c = { name = "Commit" },
-                    p = { name = "Pull" },
-                    P = { name = "Push" },
-                    b = { name = "Branches" }
-                },
-                n = { name = "+Notification" },
-                r = { name = "+Harpoon",
-                    a = { name = "Add file" },
-                    o = { name = "Open menu" },
-                    Ctrl1 = { name = "Navigate to file 1" },
-                    Ctrl2 = { name = "Navigate to file 2" },
-                    Ctrl3 = { name = "Navigate to file 3" },
-                    Ctrl4 = { name = "Navigate to file 4" },
-                },
-                s = { name = "+Telescope",
-                    f = { name = "File" },
-                    g = { name = "Grep" },
-                    s = { name = "Telescope" },
-                    c = { name = "Commits" }
-                },
-                l = { name = "+Lazy",
-                    g = { name = "Git" },
-                    d = { name = "Docker" },
-                    z = { name = "Lazy" }
-                },
-                -- stylua: ignore
-            }, { prefix = "<leader>", mode = { "n", "v" } })
-        end,
+        layout = {
+            -- height = { min = 4, max = 25 }, -- min and max height of the columns
+            width = { min = 20, max = 50 }, -- min and max width of the columns
+            spacing = 3,                    -- spacing between columns
+            -- align = "left",                 -- align columns left, center or right
+        },
+        spec = {
+            { "<leader>/", function()
+                require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                    winblend = 10,
+                    previewer = false,
+                }))
+            end, desc = "Telescope search in current buffer", mode = "n" },
+            { "<leader><leader>", require("telescope.builtin").buffers, desc="Telescope in existing buffers"},
+            { "<leader>0", group = "+Terminal" },
+            { "<leader>b", group = "+Database", icon="" },
+            { "<leader>g", group = "+Git"},
+            { "<leader>n", group = "+Notifications" },
+            { "<leader>l", group = "+Lazy", icon="󰒲" },
+            { "<leader>c", group = "+Custom", icon="󱁤" },
+            { "<leader>s", group = "+Telescope" },
+            { "<leader>r", group = "+Harpoon", icon="󰛢"},
+            { "<leader>u", group = "+Undo", icon=""},
+            { "<leader>m", group = "+Mason", icon="󱁤"},
+            { "<leader>x", group = "+Todo", icon=""},
+            { "<leader>-", require("oil").toggle_float, desc="Toggle Oil menu", icon = "󰏇" },
+            { "<leader>?", icon = "" },
+            { "<leader>ms", icon = "󱁤" },
+            { "<leader>d", hidden="true" },
+            { "<leader>p", hidden="true" },
+            { "<leader>y", hidden="true" },
+            { "<leader>Y", hidden="true" },
+        },
+      },
+      keys = {
+        {
+            "<leader>?",
+            function()
+            require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
     },
 }
