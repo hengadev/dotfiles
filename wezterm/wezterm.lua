5ae0d93aef047759048364b885db4103f1c5dd0d
GaryHY <henry.gary@hotmail.com>
2025-01-14 17:59:47 +0100

    feat: change wezterm light color theme to be flexoki-light

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 config.colors = {}
@@ -15,25 +15,25 @@ config.colors = {}
 
 -- toggle light/dark scheme with CTRL+l (found this code here: https://github.com/wez/wezterm/discussions/1118)
 wezterm.on("toggle-dark-mode", function(window, pane)
-  local light_scheme = "Rosé Pine Dawn (base16)"
-  local dark_scheme = ""
-  local overrides = window:get_config_overrides() or {}
-  wezterm.log_info("Current color scheme is: ", overrides.color_scheme)
-  if (overrides.color_scheme == light_scheme)
-  then
-  wezterm.log_info("Setting to Dark Scheme: ", overrides.color_scheme)
-  overrides.color_scheme = dark_scheme
-
-  else
-  wezterm.log_info("Setting to Light ", overrides.color_scheme)
-  overrides.color_scheme = light_scheme
-  end
-  window:set_config_overrides(overrides)
+    -- local light_scheme = "Rosé Pine Dawn (base16)"
+    local light_scheme = "flexoki-light"
+    local dark_scheme = ""
+    local overrides = window:get_config_overrides() or {}
+    wezterm.log_info("Current color scheme is: ", overrides.color_scheme)
+    if (overrides.color_scheme == light_scheme)
+    then
+        wezterm.log_info("Setting to Dark Scheme: ", overrides.color_scheme)
+        overrides.color_scheme = dark_scheme
+    else
+        wezterm.log_info("Setting to Light ", overrides.color_scheme)
+        overrides.color_scheme = light_scheme
+    end
+    window:set_config_overrides(overrides)
 end)
 
 config.keys = {
     {
-        key="l", mods="CTRL", action=wezterm.action{EmitEvent="toggle-dark-mode"},
+        key = "l", mods = "CTRL", action = wezterm.action { EmitEvent = "toggle-dark-mode" },
     },
 }
 
@@ -43,9 +43,9 @@ config.hide_tab_bar_if_only_one_tab = true
 
 
 config.font = wezterm.font_with_fallback {
-  'FiraCode Nerd Font',
-  'Berkeley Mono',
-  'nonicons',
+    'FiraCode Nerd Font',
+    'Berkeley Mono',
+    'nonicons',
 }
 
 -- TODO: change that depending on the screen you  use
@@ -58,10 +58,10 @@ config.use_fancy_tab_bar = false
 -- I don't like putting anything at the ege if I can help it.
 config.enable_scroll_bar = false
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
 
 config.tab_bar_at_bottom = true
