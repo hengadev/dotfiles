cb2d1dcc946bd00dda5d0bce2051f2f084284f34
GaryHY <henry.gary@hotmail.com>
2025-01-19 13:06:58 +0100

    feat: add avante folder to create provider specific files

---
+++ nvim/lua/plugins/avante/init.lua
@@ -0,0 +1,45 @@
+-- NOTE: here are the configuration
+local mistral = require("plugins.avante.mistral")
+-- local ollama = require("plugings.avante.ollama")
+
+return {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    opts = mistral,
+    -- note: if you want to download pre-built binary, then pass source=false. make sure to follow instruction above.
+    -- also note that downloading prebuilt binary is a lot faster comparing to compiling from source.
+    -- build = ":avantebuild source=false",
+    build = "make",
+    dependencies = {
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "muniftanjim/nui.nvim",
+        --- the below dependencies are optional,
+        "echasnovski/mini.icons",
+        -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+        { -- support for image pasting
+            "hakonharnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true,
+                    },
+                    -- required for windows users
+                    use_absolute_path = true,
+                },
+            },
+        },
+        {
+            -- make sure to set this up properly if you have lazy=true
+            "meanderingprogrammer/render-markdown.nvim",
+            opts = {
+                file_types = { "markdown", "avante" },
+            },
+            ft = { "markdown", "avante" },
+        },
+    },
+}
