987b999ae90ca6f38e4aef1351d22ab8003b8aa9
GaryHY <henry.gary@hotmail.com>
2025-01-19 19:37:03 +0100

    refactor(nvim): remove unecessary code in lsp/init.lua

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -29,15 +29,6 @@ return {
         local lsp_attach = require("plugins.lsp.attach_autocommand")
         lsp_attach()
 
-        -- LSP servers and clients are able to communicate to each other what features they support.
-        --  By default, Neovim doesn't support everything that is in the LSP specification.
-        --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-        --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-
-        -- NOTE: the old way with nvim cmp in case it does not work
-        -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-        -- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
         -- Enable the following language servers
         --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
         local servers = require("plugins.lsp.servers")
@@ -69,8 +60,5 @@ return {
                 end,
             },
         })
-
-        -- TODO: use blink to get the capabilities as set in the example
-        -- local capabilities = require("blink.cmp").get_lsp_capabilities()
     end,
 }
