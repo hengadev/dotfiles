20682ec90c5ae228b8b486fc50bb86f55f15553a
GaryHY <henry.gary@hotmail.com>
2025-01-19 16:04:52 +0100

    refactor(nvim): add folder to modularize LSP configuration

---
+++ nvim/lua/plugins/lsp/init.lua
@@ -0,0 +1,69 @@
+local mason_cmd = "Mason"
+
+return {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        -- Automatically install LSPs and related tools to stdpath for Neovim
+        { "williamboman/mason.nvim",                   cmd = mason_cmd },
+        { "williamboman/mason-lspconfig.nvim",         cmd = mason_cmd, event = { "BufReadPre", "BufNewFile" } },
+        { "WhoIsSethDaniel/mason-tool-installer.nvim", cmd = mason_cmd, event = { "BufReadPre", "BufNewFile" } },
+        -- Useful status updates for LSP.
+        { "j-hui/fidget.nvim",                         opts = {},       event = { "BufReadPre", "BufNewFile" } },
+        -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+        -- used for completion, annotations and signatures of Neovim apis
+        {
+            "folke/neodev.nvim",
+            config = true,
+            opts = {},
+            event = { "BufReadPre", "BufNewFile" }
+        },
+    },
+    config = function()
+        local lspconfig = require("lspconfig")
+        --  This function gets run when an LSP attaches to a particular buffer.
+        --    That is to say, every time a new file is opened that is associated with
+        --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+        --    function will be executed to configure the current buffer.
+        local lsp_attach = require("plugins.lsp.attach_autocommand")
+        lsp_attach()
+
+        -- LSP servers and clients are able to communicate to each other what features they support.
+        --  By default, Neovim doesn't support everything that is in the LSP specification.
+        --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+        --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+        -- Enable the following language servers
+        --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+        local servers = require("plugins.lsp.servers")
+
+        -- Ensure the servers and tools above are installed
+        require("mason").setup()
+
+        -- You can add other tools here that you want Mason to install
+        -- for you, so that they are available from within Neovim.
+        local ensure_installed = vim.tbl_keys(servers or {})
+        vim.list_extend(ensure_installed, {
+            "stylua",   -- Format Lua code
+            "delve",    -- Debugger for the go programming language
+            "codelldb", -- A native debugger based on LLDB.        })
+        })
+
+        require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+        require("mason-lspconfig").setup({
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    -- This handles overriding only values explicitly passed
+                    -- by the server configuration above. Useful when disabling
+                    -- certain features of an LSP (for example, turning off formatting for tsserver)
+                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                    lspconfig[server_name].setup(server)
+                end,
+            },
+        })
+    end,
+}
---
+++ nvim/lua/plugins/lsp/servers.lua
@@ -0,0 +1,37 @@
+--  Add any additional override configuration in the following tables. Available keys are:
+--  - cmd (table): Override the default command used to start the server
+--  - filetypes (table): Override the default list of associated filetypes for the server
+--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+--  - settings (table): Override the default settings passed when initializing the server.
+--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+return {
+    taplo = {},
+    gopls = {},
+    pyright = {},
+    clangd = {},
+    rust_analyzer = {},
+    ts_ls = {},
+    svelte_language_server = {},
+    docker_compose_language_service = {},
+    dockerls = {},
+    -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+    --
+    -- Some languages (like typescript) have entire language plugins that can be useful:
+    --    https://github.com/pmizio/typescript-tools.nvim
+    -- But for many setups, the LSP (`typescript_language_server`) will work just fine
+    -- typescript_language_server = {},
+    lua_ls = {
+        -- cmd = {...},
+        -- filetypes = { ...},
+        -- capabilities = {},
+        settings = {
+            Lua = {
+                completion = {
+                    callSnippet = "Replace",
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                diagnostics = { disable = { "missing-fields" } },
+            },
+        },
+    },
+}
