52d4fa56c7c6ee150b72996d8e76ebd5419e9596
GaryHY <henry.gary@hotmail.com>
2025-04-14 20:30:33 +0200

    feat: add edgy.nvim to help manage dap-ui and toggleterm window layout

---
+++ nvim/lua/plugins/edgy.lua
@@ -0,0 +1,22 @@
+vim.opt.splitkeep = "screen"
+
+return {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    opts = {
+        bottom = {
+            {
+                ft = "toggleterm",
+                size = { height = 0.1 },
+            },
+            { ft = "dap-repl", size = { height = 0.15 }, title = " Debug REPL" },
+            { ft = "dapui_console", size = { height = 0.15 }, title = "Debug Console" },
+        },
+        right = {
+            { ft = "dapui_scopes", title = " Scopes" },
+            { ft = "dapui_watches", title = "󰂥 Watches" },
+            { ft = "dapui_stacks", title = " Stacks" },
+            { ft = "dapui_breakpoints", title = " Breakpoints" },
+        },
+    }
+}
