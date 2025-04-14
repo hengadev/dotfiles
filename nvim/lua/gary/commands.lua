292c0c8da1424313b8b2fe8f5a90f5cbd663c801
GaryHY <henry.gary@hotmail.com>
2025-04-14 18:33:53 +0200

    feat(nvim): add kulala to help test API

--- nvim/lua/gary/commands.lua
+++ nvim/lua/gary/commands.lua
@@ -2,40 +2,40 @@
 -- That plugin could actually make makefiles if the user wanted to ?
 
 local build_commands = {
-	c = "gcc -o %:p:r.o %",
-	cpp = "g++ -std=c++17 -Wall -O2 -o %:p:r.o %",
-	rust = "cargo build --release",
-	go = "go build -o %:p:r.o %",
-	typescript = "tsc %:p:r.ts",
+    c = "gcc -o %:p:r.o %",
+    cpp = "g++ -std=c++17 -Wall -O2 -o %:p:r.o %",
+    rust = "cargo build --release",
+    go = "go build -o %:p:r.o %",
+    typescript = "tsc %:p:r.ts",
 }
 
 local debug_build_commands = {
-	c = "gcc -g -o %:p:r.o %",
-	cpp = "g++ -std=c++17 -g -o %:p:r.o %",
-	rust = "cargo build",
-	go = "go build -o %:p:r.o %",
-	typescript = "tsc %:p:r.ts",
+    c = "gcc -g -o %:p:r.o %",
+    cpp = "g++ -std=c++17 -g -o %:p:r.o %",
+    rust = "cargo build",
+    go = "go build -o %:p:r.o %",
+    typescript = "tsc %:p:r.ts",
 }
 
 local run_commands = {
-	c = "%:p:r.o",
-	cpp = "%:p:r.o",
-	rust = "cargo run --release",
-	go = "%:p:r.o",
-	typescript = "node %:p:r.js",
-	javascript = "node %:p:r.js",
-	python = "python3 %:p:r.py",
-	java = "java %:p:r.java",
+    c = "%:p:r.o",
+    cpp = "%:p:r.o",
+    rust = "cargo run --release",
+    go = "%:p:r.o",
+    typescript = "node %:p:r.js",
+    javascript = "node %:p:r.js",
+    python = "python3 %:p:r.py",
+    java = "java %:p:r.java",
 }
 
 -- TODO: Make a run release command for rust specifically because I want to have run and run_release, the same way with debug and debug_release ?
 
 local function bind(op, outer_opts)
-	outer_opts = outer_opts or { noremap = true }
-	return function(lhs, rhs, opts)
-		opts = vim.tbl_extend("force", outer_opts, opts or {})
-		vim.keymap.set(op, lhs, rhs, opts)
-	end
+    outer_opts = outer_opts or { noremap = true }
+    return function(lhs, rhs, opts)
+        opts = vim.tbl_extend("force", outer_opts, opts or {})
+        vim.keymap.set(op, lhs, rhs, opts)
+    end
 end
 
 local nnoremap = bind("n")
@@ -47,47 +47,47 @@ local nnoremap = bind("n")
 -- local tnoremap = bind("t")
 
 local t = {
-	Build = { name = build_commands, keymap = "cb", desc = "Build Release" },
-	DebugBuild = { name = debug_build_commands, keymap = "cd", desc = "Build Debug" },
-	Run = { name = run_commands, keymap = "cl", desc = "Run" },
+    Build = { name = build_commands, keymap = "cb", desc = "Build Release" },
+    DebugBuild = { name = debug_build_commands, keymap = "cd", desc = "Build Debug" },
+    Run = { name = run_commands, keymap = "cl", desc = "Run" },
 }
 
 -- Make a general function to create the custom command
 local create_custom_command = function(command_name, command_table)
-	vim.api.nvim_create_user_command(command_name, function()
-		local filetype = vim.bo.filetype
+    vim.api.nvim_create_user_command(command_name, function()
+        local filetype = vim.bo.filetype
 
-		for file, command in pairs(command_table) do
-			if filetype == file then
-				if command_name == "Run" then
-					vim.cmd("sp")
-					vim.cmd("term " .. command)
-					vim.cmd("resize 10S")
-					local keys = vim.api.nvim_replace_termcodes("i", true, false, true)
-					vim.api.nvim_feedkeys(keys, "n", false)
-				else
-					vim.cmd("!" .. command)
-				end
-				break
-			end
-		end
-	end, {})
+        for file, command in pairs(command_table) do
+            if filetype == file then
+                if command_name == "Run" then
+                    vim.cmd("sp")
+                    vim.cmd("term " .. command)
+                    vim.cmd("resize 10S")
+                    local keys = vim.api.nvim_replace_termcodes("i", true, false, true)
+                    vim.api.nvim_feedkeys(keys, "n", false)
+                else
+                    vim.cmd("!" .. command)
+                end
+                break
+            end
+        end
+    end, {})
 end
 
 -- Building the functions.
 for name, command_table in pairs(t) do
-	nnoremap(
-		"<leader>" .. command_table["keymap"],
-		"<Cmd>" .. name .. "<CR>",
-		{ silent = true, desc = "Custom : " .. command_table["desc"] }
-	)
-	create_custom_command(name, command_table["name"])
+    nnoremap(
+        "<leader>" .. command_table["keymap"],
+        "<Cmd>" .. name .. "<CR>",
+        { silent = true, desc = "Custom : " .. command_table["desc"] }
+    )
+    create_custom_command(name, command_table["name"])
 end
 
 -- For rust, a combination of bulding and running release
 vim.api.nvim_create_user_command("Ha", function()
-	vim.cmd([[Build]])
-	vim.cmd([[Run]])
+    vim.cmd([[Build]])
+    vim.cmd([[Run]])
 end, {})
 nnoremap("<leader>cr", "<Cmd>Ha<CR>", { silent = true, desc = "Build and Run" })
 
---
+++ nvim/lua/plugins/kulala.lua
@@ -0,0 +1,5 @@
+return {
+    "mistweaverco/kulala.nvim",
+    ft = { "http", "rest" },
+    opts = { global_keymaps = true, },
+}
