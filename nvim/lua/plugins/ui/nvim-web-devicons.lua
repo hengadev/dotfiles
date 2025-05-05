70fe3d0dbc08b9c981d97b450792551bbaddc09e
GaryHY <henry.gary@hotmail.com>
2025-05-05 14:53:23 +0200

    feat(nvim): add some custom icons for nvim-web-devicons

--- nvim/lua/plugins/ui/nvim-web-devicons.lua
+++ nvim/lua/plugins/ui/nvim-web-devicons.lua
@@ -1,4 +1,15 @@
+local overrideDevicons = {
+    default_icon = { icon = "󰈚", name = "Default" },
+    js = { icon = "󰌞", name = "js" },
+    ts = { icon = "󰛦", name = "ts" },
+    lock = { icon = "󰌾", name = "lock" },
+    ["robots.txt"] = { icon = "󰚩", name = "robots" },
+}
+
 return {
-    -- icons
-    { "nvim-tree/nvim-web-devicons", lazy = true },
+    "nvim-tree/nvim-web-devicons",
+    lazy = false,
+    opts = function()
+        return { override = overrideDevicons }
+    end,
 }
