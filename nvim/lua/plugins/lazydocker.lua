09b045882f99d5ff8b00faf2b6f8a8bb34c9aafd
GaryHY <henry.gary@hotmail.com>
2025-10-08 12:54:00 +0200

    feat(nvim): update lazydocker plugin configuration
    Switch to mgierada/lazydocker.nvim fork
    Add curved border configuration for better UI
    Update command invocation from LazyDocker to Lazydocker

--- nvim/lua/plugins/lazydocker.lua
+++ nvim/lua/plugins/lazydocker.lua
@@ -1,11 +1,17 @@
 local map = require("utils.helpers").set_prefix("LazyDocker")
-map("<leader>ld", "<Cmd>LazyDocker<CR>", "Open menu")
+map("<leader>ld", "<Cmd>Lazydocker<CR>", "Open menu")
 
 return {
-    "crnvl96/lazydocker.nvim",
-    cmd = "LazyDocker",
+    -- "crnvl96/lazydocker.nvim",
+    "mgierada/lazydocker.nvim",
+    cmd = "Lazydocker",
     opts = {}, -- automatically calls `require("lazydocker").setup()`
     dependencies = {
         "MunifTanjim/nui.nvim",
-    }
+    },
+    config = function()
+        require("lazydocker").setup({
+            border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+        })
+    end,
 }
