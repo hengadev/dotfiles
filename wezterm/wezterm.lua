abfb74a15b58943a24eba32c33ae4e7e3036748e
GaryHY <henry.gary@hotmail.com>
2025-10-08 13:06:39 +0200

    style(wezterm): normalize code formatting
    Apply consistent indentation and quote usage across all wezterm
    configuration files for better readability and maintainability.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,15 +7,15 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Font settings
-config.font = wezterm.font_with_fallback {
-    'FiraCode Nerd Font',
-    'Berkeley Mono',
-    'nonicons',
-}
+config.font = wezterm.font_with_fallback({
+	"FiraCode Nerd Font",
+	"Berkeley Mono",
+	"nonicons",
+})
 
 config.font_size = 18
 
@@ -29,16 +29,16 @@ wezterm.on("toggle-light-mode", theme_toggler.set(wezterm, "flexoki-light", "lig
 wezterm.on("window-config-reloaded", theme_toggler.watch_theme_file(wezterm))
 
 config.keys = {
-    -- theme manual switching
-    { key = "1", mods = "CTRL", action = wezterm.action { EmitEvent = "toggle-light-mode" } },
-    { key = "2", mods = "CTRL", action = wezterm.action { EmitEvent = "toggle-dark-mode" } },
+	-- theme manual switching
+	{ key = "1", mods = "CTRL", action = wezterm.action({ EmitEvent = "toggle-light-mode" }) },
+	{ key = "2", mods = "CTRL", action = wezterm.action({ EmitEvent = "toggle-dark-mode" }) },
 }
 
 -- Appearance settings
 config.hide_tab_bar_if_only_one_tab = true -- hide the bar, if only one tab, useful since I use tmux
-config.use_fancy_tab_bar = false           -- default is true, has more "native" look
+config.use_fancy_tab_bar = false -- default is true, has more "native" look
 config.enable_scroll_bar = false
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.tab_bar_at_bottom = true
 config.freetype_load_target = "HorizontalLcd"
 
