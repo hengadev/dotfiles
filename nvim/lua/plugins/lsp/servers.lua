521b6cf141ab22b8352ca971a1f86272842bfb96
GaryHY <henry.gary@hotmail.com>
2026-01-06 13:05:37 +0100

    fix(nvim): correct tailwindcss LSP server name
    Change from incorrect 'tailwindcss_language_server' to correct
    'tailwindcss' to match lspconfig naming convention.

--- nvim/lua/plugins/lsp/servers.lua
+++ nvim/lua/plugins/lsp/servers.lua
@@ -16,7 +16,7 @@ return {
     docker_compose_language_service = {},
     dockerls = {},
     terraformls = {},
-    tailwindcss_language_server = {},
+    tailwindcss = {},
     -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
